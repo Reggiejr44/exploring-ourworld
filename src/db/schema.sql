157bac44ca081ae0bd1e1fbb19d8f4a3d8fe405e
colint771 <colint771@gmail.comgit config --global user.email colint771@gmail.com>
2023-02-15 23:59:33 -0500

    css styling updates, readme, background image

---
+++ src/db/schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS ourworld_db;
+
+CREATE DATABASE ourworld_db;
+
---
+++ src/models/Maps.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Maps extends Model {}
+
+Maps.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    city_name: { // City Name gathered from coordinates?
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    city_state: { // State Name gathered from coordinates?
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    map_coordinates_lat: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    map_coordinates_lon: {
+      type: DataTypes.FLOAT,
+        allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "maps",
+  }
+);
+
+module.exports = Maps;
