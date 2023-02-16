cd897bf418afee62ff9c20414e523d35248525b0
Reggiejr44 <115569377+Reggiejr44@users.noreply.github.com>
2023-02-16 18:04:28 -0500

    edited const and files

--- src/seeds/maps.js
+++ src/seeds/maps.js
@@ -2,10 +2,10 @@ const { Maps } = require('../models');
 
 const mapsData = [
     {
-        city_name: 'DEFAULT',
-        city_state: 'DEFAULT',
-        map_coordinates_lat: '31.1957',
-        map_coordinates_lon: '-98.7181'
+        city_name: '',
+        city_state: '',
+        map_coordinates_lat: '',
+        map_coordinates_lon: ''
     },
 ];
 
