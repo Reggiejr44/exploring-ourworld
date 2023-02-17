ab7328d34c9163b5d6f4909e47ad3f67869fd758
Reggiejr44 <115569377+Reggiejr44@users.noreply.github.com>
2023-02-16 20:30:27 -0500

    secret db

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const app = express();
   const hbs = exphbs.create({ helpers });
 
   const sessionConfig = {
-    secret: config.DB_SECRET,
+    secret: process.env.DB_SECRET,
     cookie: {
       maxAge: 300000,
       httpsOnly: true,
